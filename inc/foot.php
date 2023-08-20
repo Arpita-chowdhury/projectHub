f3e0db668362c12db3039b6d330f5e86039e5343
Arpita Chowdhury <65353539+Arpita-chowdhury@users.noreply.github.com>
2023-08-20 23:14:15 +0600

    including file parts uploaded

---
+++ inc/foot.php
@@ -0,0 +1,19 @@
+<!--footer section inclusion-->
+
+            <footer class="bg-secondary text-center text-light py-3 mt-4">
+                <div> <span class="h6">PROJECT</span><span class="fw-light small">HUB</span> </div>
+                <p class="font-monospace">Project Management System and Repository <br>
+                <small>Department Of Computer Science and Engineering, Leading University, Sylhet</small>
+                </p>
+            </footer>
+
+        <!--The mother container div ending tag-->
+        </div>
+
+        <!--Bootstrap JS cdn-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+        
+    
+    </body>
+
+</html>
---
+++ inc/head.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ProjectHub</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+    <body>
+
+        <!--The mother container div start-->
+        <div class="container">
+
+            <!--navbar starts here-->
+            <header>
+                <nav class="navbar navbar-expand-sm sticky-top bg-secondary px-2">
+
+                    <!--logo design-->
+                    <a class="navbar-brand d-flex text-capitalize" href="index.php" >
+                        <h3><span class=" fw-bold bg-dark text-light px-2  rounded">p</span></h3>
+                        <h3><span class=" fw-bold  bg-dark text-light px-2  rounded">r</span></h3>
+                        <h3><span class="  fw-bold  bg-dark text-light px-2  rounded">o</span></h3>
+                        <h3><span class="  fw-bold bg-dark text-light px-2  rounded">j</span></h3>
+                        <h3><span class="  fw-bold bg-dark text-light px-2  rounded">e</span></h3>
+                        <h3><span class=" fw-bold  bg-dark text-light px-2 rounded">c</span></h3>
+                        <h3><span class="  fw-bold  bg-dark text-light px-2  rounded">t</span></h3>
+                        <span><h4><sub class="text-light bg-dark rounded px-1">HUB</sub></h4></span>
+                    </a>
+                    <!--logo design-->
+
+
+                    <!--navbar collapse button for small screen-->
+                    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon "></span>
+                    </button>
+
+                    <!--navbar links-->
+                    <div class="collapse navbar-collapse" id="navbarContent">
+                        <ul class="navbar-nav ms-auto ">
+                            <li class="nav-item "><a class="nav-link text-light my-sidebar-style" href="notification.php"><i class="fa-solid fa-bell my-sidebar-style"></i></a></li>
+                            <li class="nav-item "><a class="nav-link text-light my-sidebar-style" href="login.php">Log In</a></li>
+                            <li class="nav-item" ><a class="nav-link text-light my-sidebar-style" href="registration.php">Register</a></li>
+                        </ul>
+                    </div>
+                    
+                </nav>
+            </header>
+            <!--navbar ends here-->
+
